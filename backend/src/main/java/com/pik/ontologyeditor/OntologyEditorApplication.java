e7ad2b1dad5ce6be2c61ca5c4b5744745ee139f5
Paweł Lech <pawellechtoja@gmail.com>
2021-05-09 17:38:15 +0200

    #2 pierwsza wersja uploadu pliku z wykorzystaniem axios

--- backend/src/main/java/com/pik/ontologyeditor/OntologyEditorApplication.java
+++ backend/src/main/java/com/pik/ontologyeditor/OntologyEditorApplication.java
@@ -1,17 +1,25 @@
 package com.pik.ontologyeditor;
 
+import javax.annotation.Resource;
+
+import com.pik.ontologyeditor.service.FilesStorageService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
-public class OntologyEditorApplication {
+public class OntologyEditorApplication implements CommandLineRunner {
+	@Resource
+	FilesStorageService storageService;
 
 	public static void main(String[] args) {
-		try {
-			SpringApplication.run(OntologyEditorApplication.class, args);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+		SpringApplication.run(OntologyEditorApplication.class, args);
 	}
 
+	@Override
+	public void run(String... arg) throws Exception {
+		storageService.deleteAll();
+		storageService.init();
+	}
 }
